0c3f24725d00b09e042ca64646198e19cc6b1843
datanglibai <yuzi_801212@sina.com.cn>
2018-06-12 16:27:34 +0800

    update

--- miscs/numstring.ts
+++ miscs/numstring.ts
@@ -1,13 +1,23 @@
-function formatNumberWithComma(num: number) {
-    let numStr = Number.isInteger(num) ? num.toString() : num.toFixed(2);
-    return numStr.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+// function formatNumberWithComma(num: number) {
+//     let numStr = Number.isInteger(num) ? num.toString() : num.toFixed(2);
+//     return numStr.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-}
+// }
 
 
-console.log(formatNumberWithComma(100));
-console.log(formatNumberWithComma(100.22));
-console.log(formatNumberWithComma(8100));
-console.log(formatNumberWithComma(8100.45));
-console.log(formatNumberWithComma(72548100.0));
-console.log(formatNumberWithComma(72548100.45));
+// console.log(formatNumberWithComma(100));
+// console.log(formatNumberWithComma(100.22));
+// console.log(formatNumberWithComma(8100));
+// console.log(formatNumberWithComma(8100.45));
+// console.log(formatNumberWithComma(72548100.0));
+// console.log(formatNumberWithComma(72548100.45));
+
+// let res1 = NaN;
+// let res2 = 87384.34;
+// console.log("res1: ", res1 !== NaN, "res2: ", res2 !== NaN);
+// console.log(isNaN(res1), isNaN(res2));
+
+let test = ['a', 'b', 'c']
+
+for (const v of test)
+    console.log(v);
